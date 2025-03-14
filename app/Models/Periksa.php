6bbcb94f011850613f097165fe21b0f33687add0
Akbarlion <muhammadakbarlion@gmail.com>
2025-03-14 09:37:28 +0700

    Add Table Migrations

---
+++ app/Models/Periksa.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Periksa extends Model
+{
+    //
+    use HasFactory;
+
+    protected $table = 'periksa';
+
+    protected $fillable = [
+        'id_pasien',
+        'id_dokter',
+        'tgl_periksa',
+        'catatan',
+        'biaya_periksa',
+    ];
+
+    public function pasien(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_pasien');
+    }
+
+    public function dokter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_dokter');
+    }
+
+    public function detailPeriksa(): HasMany
+    {
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa');
+    }
+
+}
